ec165df99cb03cc0d183e4d90693da169162d0c8
theZebraBatman <fgportman00@gmail.com>
2018-06-01 14:43:13 -0400

    Created FRONT_TO_BACK_RATIO in Mecanum classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTemplates/Mecanum2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTemplates/Mecanum2.java
@@ -1,15 +1,18 @@
 package org.firstinspires.ftc.teamcode.robotTemplates;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 /**
  * Created by Frank Portman on 5/21/2018
  */
 public class Mecanum2 extends MecanumDT {
+    DcMotor lf, la, rf, ra;
     public Mecanum2(){
-        super(0.01);
+        super(0.01, 1);
         maxSpeed = 1;
     }
     public Mecanum2(double brakePow, double speed) {
-        super(brakePow);
+        super(brakePow, 1);
         maxSpeed = speed;
     }
     @Override
@@ -25,5 +28,43 @@ public class Mecanum2 extends MecanumDT {
     @Override
     public void loop() {
         super.loop();
+    }
+    public void  initMotors(){
+        rf = hardwareMap.dcMotor.get("rf");
+        lf = hardwareMap.dcMotor.get("lf");
+        la = hardwareMap.dcMotor.get("la");
+        ra = hardwareMap.dcMotor.get("ra");
+        rf.setDirection(REVERSE);
+        ra.setDirection(REVERSE);
+        lf.setDirection(FORWARD);
+        la.setDirection(FORWARD);
+        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.UNKNOWN);
+        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.UNKNOWN);
+        la.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.UNKNOWN);
+        ra.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.UNKNOWN);
+    }
+    //sets the motor powers to the specified values
+    public void refreshMotors(double I, double II, double III, double IV){
+        rf.setPower(I);
+        lf.setPower(II);
+        la.setPower(III);
+        ra.setPower(IV);
+    }
+    //sets the motor powers to the specified values at the specified speed
+    public void refreshMotors(double I, double II, double III, double IV, double speed){
+        rf.setPower(speed * I);
+        lf.setPower(speed * II);
+        la.setPower(speed * III);
+        ra.setPower(speed * IV);
+    }
+    //sets the motor powers to rightForePow and leftForePow respectively
+    public void refreshMotors(){
+        setPower(rf, rightForePow);
+        setPower(la, leftAftPow);
+        setPower(lf, leftForePow);
+        setPower(ra, rightAftPow);
+    }
+    public void brake(){
+        refreshMotors(brakePow, brakePow, -brakePow, -brakePow);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTemplates/MecanumDT.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTemplates/MecanumDT.java
@@ -8,15 +8,16 @@ import org.firstinspires.ftc.teamcode.robotUniversal.Vector2;
 /**
  * Created by Frank Portman on 5/21/2018
  */
-public class MecanumDT extends HolonomicDT {
-    DcMotor lf, la, rf, ra;
+public abstract class MecanumDT extends HolonomicDT {
     public double       leftForePow,
                         rightForePow,
                         leftAftPow,
                         rightAftPow,
                         angleBetween;
-    public MecanumDT(double brakePow){
+    public final double FRONT_TO_BACK_RATIO;
+    public MecanumDT(double brakePow, double ratio){
         super(brakePow);
+        FRONT_TO_BACK_RATIO  = ratio;
     }
     public void init(){
         super.init();
@@ -28,7 +29,7 @@ public class MecanumDT extends HolonomicDT {
 
     //returns the power of the left rear and right fore motors needed to drive at a given angle at a given speed
     public double getRightForePow(double ang, double speed){
-        return Math.sin(ang + Math.PI / 4) * speed / 2;
+        return Math.sin(ang + Math.PI / 4) * FRONT_TO_BACK_RATIO * speed / 2;
     }
 
     //returns the power of the left rear and right fore motors needed to drive along a given vector
@@ -38,7 +39,7 @@ public class MecanumDT extends HolonomicDT {
 
     //returns the power of the left fore and right rear motors needed to drive at a given angle at a given speed
     public double getLeftForePow(double ang, double speed){
-        return Math.cos(ang + Math.PI / 4) * speed / 2;
+        return Math.cos(ang + Math.PI / 4) * FRONT_TO_BACK_RATIO * speed / 2;
     }
     //returns the power of the left fore and right rear motors needed to drive along a given vector
     public double getLeftForePow(Vector2 vel){
@@ -48,34 +49,16 @@ public class MecanumDT extends HolonomicDT {
     public void setVelocity(double ang, double speed){
         rightForePow = getRightForePow(ang, speed);
         leftForePow = getLeftForePow(ang, speed);
-        leftAftPow = rightForePow;
-        rightAftPow = leftForePow;
+        leftAftPow = rightForePow / FRONT_TO_BACK_RATIO;
+        rightAftPow = leftForePow / FRONT_TO_BACK_RATIO;
+        rightForePow *= FRONT_TO_BACK_RATIO;
+        leftForePow *= FRONT_TO_BACK_RATIO;
     }
     //sets the power of the motors in order to drive at a given velocity
     public void setVelocity(Vector2 vel){
         setVelocity(vel.angle(), vel.magnitude());
     }
-    //sets the motor powers to the specified values
-    public void refreshMotors(double I, double II, double III, double IV){
-        rf.setPower(I);
-        lf.setPower(II);
-        la.setPower(III);
-        ra.setPower(IV);
-    }
-    //sets the motor powers to the specified values at the specified speed
-    public void refreshMotors(double I, double II, double III, double IV, double speed){
-        rf.setPower(speed * I);
-        lf.setPower(speed * II);
-        la.setPower(speed * III);
-        ra.setPower(speed * IV);
-    }
-    //sets the motor powers to rightForePow and leftForePow respectively
-    public void refreshMotors(){
-        setPower(rf, rightForePow);
-        setPower(la, leftAftPow);
-        setPower(lf, leftForePow);
-        setPower(ra, rightAftPow);
-    }
+
     //sets the turnMult variable
     public void setTurnMult(double tm){
         turnMult = tm;
@@ -104,23 +87,6 @@ public class MecanumDT extends HolonomicDT {
         setTurnPow();
         normalizeMotors();
         refreshMotors();
-    }
-    public void brake(){
-        refreshMotors(brakePow, brakePow, -brakePow, -brakePow);
-    }
-    public void  initMotors(){
-        rf = hardwareMap.dcMotor.get("rf");
-        lf = hardwareMap.dcMotor.get("lf");
-        la = hardwareMap.dcMotor.get("la");
-        ra = hardwareMap.dcMotor.get("ra");
-        rf.setDirection(REVERSE);
-        ra.setDirection(REVERSE);
-        lf.setDirection(FORWARD);
-        la.setDirection(FORWARD);
-        rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.UNKNOWN);
-        lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.UNKNOWN);
-        la.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.UNKNOWN);
-        ra.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.UNKNOWN);
     }
 
 }
