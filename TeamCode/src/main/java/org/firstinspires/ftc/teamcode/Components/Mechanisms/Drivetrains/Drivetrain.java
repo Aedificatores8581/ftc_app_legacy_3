d18cc373e2736e8ef0fcbe8c812674fe766a9983
theZebraBatman <fgportman00@gmail.com>
2018-06-01 14:09:15 -0400

    Added code to dev_proposal branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Mechanisms/Drivetrains/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Mechanisms/Drivetrains/Drivetrain.java
@@ -1,37 +1,30 @@
-package org.firstinspires.ftc.teamcode.robotTemplates;
+package org.firstinspires.ftc.teamcode.Components.Mechanisms.Drivetrains;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.robotUniversal.Vector2;
 
 /**
- * Created by Frank Portman on 5/21/2018
+ * Created by Frank Portman on 5/29/2018
  */
-import java.util.ArrayList;
-
-public abstract class Drivetrain extends Robot {
+public abstract class Drivetrain {
     public final DcMotor.Direction FORWARD = DcMotor.Direction.FORWARD, REVERSE = DcMotor.Direction.REVERSE;
     public double minTurn;
     public double brakePow;
     public double maxSpeed = 1;
+    public Vector2 turnVector;
+    public Vector2 robotAngle;
+    public Vector2 speedVector;
     public Drivetrain(double pow){
-        super(false);
         brakePow = pow;
     }
     //Maps the drive motors to the rev module
     public abstract void initMotors();
-    @Override
-    public void init(){
-        super.init();
-    }
     //gives the motors holding power
     public abstract void brake();
     public enum Direction{
         FOR,
         BACK
-    }
-    public void start(){
-        super.start();
     }
     public void setPower(DcMotor m, double pow){
         m.setPower(pow *  maxSpeed);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Mechanisms/Drivetrains/HolonomicDrivetrains/MecanumDT.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.Components.Mechanisms.Drivetrains.HolonomicDrivetrains;
+
+import org.firstinspires.ftc.teamcode.Components.Mechanisms.Drivetrains.Drivetrain;
+
+public abstract class MecanumDT extends Drivetrain {
+
+}
