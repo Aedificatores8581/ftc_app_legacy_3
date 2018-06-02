59d3f86eed9c9742aa25484ecde4e4c18671bdae
theZebraBatman <fgportman00@gmail.com>
2018-06-02 15:49:14 -0400

    Updated and added comments to PIDController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/PIDController.java
@@ -7,13 +7,16 @@ public class PIDController {
     public double       error,
                         setpoint,
                         processVar,
-                        integral = 0,
+                        integral      = 0,
                         deltaTime,
                         derivative,
                         prevError,
                         currentOutput,
                         time,
-                        gain;
+                        TI            = 0,
+                        TD            = 0,
+                        gain,
+                        currentTime;
     public final double KP;
     public final double KI;
     public final double KD;
@@ -25,31 +28,45 @@ public class PIDController {
         deltaTime = tc;
         DESIRED_SSE = sse;
     }
-    public double loop(double setpoint){
-        error = setpoint - processVar;
-        integral += error * deltaTime;
-        derivative = (error - prevError) / deltaTime;
-        currentOutput = KP * error + KI * integral + KD * derivative;
-        time = System.currentTimeMillis();
-        prevError = error;
-        return currentOutput;
+    //Sets TI and TD to the given parameters
+    public void setStandardForm(double integralTime, double derivativeTime){
+        TI = integralTime;
+        TD = derivativeTime;
     }
-    public void loop(){
+    //one iteration of an ideal PID loop
+    public void idealLoop(){
         if(time + deltaTime >= System.currentTimeMillis()) {
             error = setpoint - processVar;
             integral += error * deltaTime;
             derivative = (error - prevError) / deltaTime;
             time = System.currentTimeMillis();
-            gain = currentOutput;
+            double temp = currentOutput;
             currentOutput = KP * error + gain() - DESIRED_SSE + KI * integral + KD * derivative;
+            gain = currentOutput - temp;
+            prevError = error;
+        }
+    }
+    //one iteration of a standard PID loop
+    public void standardLoop(){
+        if(time + deltaTime >= currentTime) {
+            error = setpoint - processVar;
+            if(time + TI >= currentTime)
+                integral += error * deltaTime;
+            if(time + TD >= currentTime)
+                derivative = (error - prevError) / deltaTime;
+            time = System.currentTimeMillis();
+            double temp = currentOutput;
+            currentOutput = KP * error + gain() - DESIRED_SSE + KP / TI * integral + KP * TD * derivative;
             gain = currentOutput - gain;
             prevError = error;
         }
-        loop(currentOutput);
+        currentTime = System.currentTimeMillis();
     }
-    public double gain(){
+    //calculates the gain of the loop
+    public double gain() {
         return transferFunction() / (1 + gain * transferFunction());
     }
+    //returns the value returned by the loop's transfer function
     public double transferFunction(){
         return currentOutput / (setpoint - gain * currentOutput);
     }
