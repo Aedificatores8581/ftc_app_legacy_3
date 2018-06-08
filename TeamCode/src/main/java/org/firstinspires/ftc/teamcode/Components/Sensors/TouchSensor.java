690337b9b4d60474b7b3c6667d739f2557c41184
theZebraBatman <fgportman00@gmail.com>
2018-06-08 17:46:22 -0400

    Created the TouchSensor class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sensors/TouchSensor.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.Components.Sensors;
+
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Created by Frank Portman on 6/8/2018
+ */
+public class TouchSensor {
+    public DigitalChannel touchSensor;
+    public void init(HardwareMap hardwareMap, String name){
+        touchSensor = hardwareMap.digitalChannel.get(name);
+        touchSensor.setMode(DigitalChannel.Mode.INPUT);
+    }
+    public boolean isPressed(){
+        return touchSensor.getState();
+    }
+}
