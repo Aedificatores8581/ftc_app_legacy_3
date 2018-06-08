85b8cb3f3840b727c3cfe47412ecb02664ad87a3
theZebraBatman <fgportman00@gmail.com>
2018-06-08 17:53:20 -0400

    Created MagneticLimitSwitch class and added toString method to TouchSensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sensors/TouchSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sensors/TouchSensor.java
@@ -7,12 +7,15 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * Created by Frank Portman on 6/8/2018
  */
 public class TouchSensor {
-    public DigitalChannel touchSensor;
+    private DigitalChannel touchSensor;
     public void init(HardwareMap hardwareMap, String name){
         touchSensor = hardwareMap.digitalChannel.get(name);
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
     }
     public boolean isPressed(){
         return touchSensor.getState();
+    }
+    public String toString(){
+        return isPressed() ? "pressed" : "released";
     }
 }
