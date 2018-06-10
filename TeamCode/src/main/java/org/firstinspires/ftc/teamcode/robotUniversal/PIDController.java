d72c15637dac89fb8f05d424ce14821b8ffc9741
theZebraBatman <fgportman00@gmail.com>
2018-06-10 15:22:48 -0400

    Added and implemented integralMax in PID class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/PIDController.java
@@ -4,26 +4,34 @@ package org.firstinspires.ftc.teamcode.robotUniversal;
  * Created by Frank Portman on 6/2/2018
  */
 public class PIDController {
-    public double       error        ,
-                        setpoint     ,
-                        processVar   ,
-                        integral      = 0,
-                        deltaTime    ,
-                        derivative   ,
-                        prevError    ,
-                        currentOutput,
-                        time         ,
-                        TI            = 0,
-                        TD            = 0,
-                        currentTime  ;
-    public final double KP           ;
-    public final double KI           ;
-    public final double KD           ;
+    public double error        ,
+                  setpoint     ,
+                  processVar   ,
+                  integral      = 0,
+                  deltaTime    ,
+                  derivative   ,
+                  prevError    ,
+                  currentOutput,
+                  time         ,
+                  TI            = 0,
+                  TD            = 0,
+                  currentTime  ,
+                  integralMax   = Double.POSITIVE_INFINITY;
+    public double KP            = 0;
+    public double KI            = 0;
+    public double KD            = 0;
     public PIDController(double kp, double ki, double kd, double tc){
         KP = kp;
         KI = ki;
         KD = kd;
         deltaTime = tc;
+    }
+    public PIDController(double kp, double ki, double kd, double tc, double im){
+        KP = kp;
+        KI = ki;
+        KD = kd;
+        deltaTime = tc;
+        integralMax = im;
     }
     //Sets TI and TD to the given parameters
     public void setStandardForm(double integralTime, double derivativeTime){
@@ -35,6 +43,7 @@ public class PIDController {
         if(time + deltaTime >= System.currentTimeMillis()) {
             error = setpoint - processVar;
             integral += error * deltaTime;
+            integral = integralMax > integral ? integral : integralMax;
             derivative = (error - prevError) / deltaTime;
             time = System.currentTimeMillis();
             currentOutput = KP * error + KI * integral + KD * derivative;
@@ -55,5 +64,6 @@ public class PIDController {
         }
         currentTime = System.currentTimeMillis();
     }
+
 
 }
