8fbcc85ffe8b44d937ca2fe468d0147595b35147
mayadapples <tlovinski@outlook.com>
2018-06-24 16:05:26 -0400

    Made RobitBot Override

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/ZoidbergV1/RobitBot.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.Robots.ZoidbergV1;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.Components.Mechanisms.Drivetrains.TankDrivetrains.TankDT;
+import org.firstinspires.ftc.teamcode.Robots.Robot;
+import org.firstinspires.ftc.teamcode.robotUniversal.Vector2;
+
+public abstract class RobitBot extends Robot {
+
+    public TankDT drivetrain = new TankDT() {
+        public void initMotors(HardwareMap map) {
+            leftMotor = map.dcMotor.get("left");
+            rightMotor = map.dcMotor.get("right");
+
+            leftMotor.setDirection(FORWARD);
+            rightMotor.setDirection(REVERSE);
+
+
+        }
+
+        public void normalizeMotors() {}
+
+        public DcMotor leftMotor, rightMotor;
+
+        public void teleOpLoop(Vector2 leftVect, Vector2 rightVect, Vector2 angle) {
+            switch (controlState) {
+                // TODO: Implement Field-Centric and Field-Centric Vector.
+
+                case ARCADE:
+                    turnMult = 1 - leftVect.magnitude() * (1 - maxTurn);
+                    leftPow = leftVect.y + turnMult * rightVect.x;
+                    rightPow = leftVect.y - turnMult * rightVect.x;
+                    break;
+
+                case TANK:
+                    leftPow = leftVect.y;
+                    rightPow = rightVect.y;
+                    break;
+            }
+            setLeftPow(-leftPow);
+            setRightPow(-rightPow);
+        }
+
+        //TODO_OVERRIDE!
+        public Direction setDirection(){
+            if(leftPow + rightPow > 0)
+                direction = Direction.FOR;
+            else if(leftPow != rightPow)
+                direction = Direction.BACK;
+            return direction;
+        }
+
+        public void setLeftPow(double pow) {
+            leftPow = pow;
+            leftMotor.setPower(leftPow);
+        }
+
+        public void setRightPow(double pow) {
+            rightPow = pow;
+            rightMotor.setPower(rightPow);
+        }
+    };
+
+    @Override
+    public void start() {super.start();}
+
+    @Override
+    public void init() {super.init();}
+}
