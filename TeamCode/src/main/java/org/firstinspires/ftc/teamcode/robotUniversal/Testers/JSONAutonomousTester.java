0328de0115d217f9192fb8261e099179fa57328b
Mister-Minister-Master <misterministermaster@gmail.com>
2018-07-29 14:09:39 -0400

    Created JSONAutonGetter and tester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotUniversal/Testers/JSONAutonomousTester.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.robotUniversal.Testers;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.robotUniversal.JSONAutonGetter;
+import org.firstinspires.ftc.teamcode.robotUniversal.TelemetryLogger;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+/*
+* A Tester Opmode for JSONAutonGetter
+* TODO: MAKE BETTER CLASS NAME FOR "JSONAutonGetter"
+* */
+@Autonomous(name = "Autonomous with JSON", group = "testers")
+public class JSONAutonomousTester extends OpMode {
+    JSONAutonGetter jsonHandler;
+    int x, y;
+
+
+    public void init(){
+        try {
+            jsonHandler = new JSONAutonGetter("JSONAutonomousTester.json");
+        } catch(IOException | JSONException e) {
+            telemetry.addLine("Couldn't init JSON file handler: " + e.getMessage());
+        }
+
+        try {
+            x = jsonHandler.jsonObject.getInt("x");
+        } catch (JSONException e) {
+            telemetry.addLine("Couldn't access 'x': " + e.getMessage());
+        }
+        
+        try {
+            y = jsonHandler.jsonObject.getInt("y");
+        } catch (JSONException e) {
+            telemetry.addLine("Couldn't access 'y': " + e.getMessage());
+        }
+    }
+
+    public void loop(){
+        telemetry.addData("X + Y = ", x+y);
+    }
+}
+
