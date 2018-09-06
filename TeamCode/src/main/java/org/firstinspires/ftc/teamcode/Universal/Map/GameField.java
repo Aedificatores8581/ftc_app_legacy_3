f09c3568fe20a1284d5a75f06a643571973f782d
theZebraBatman <fgportman00@gmail.com>
2018-09-06 13:31:43 -0400

    Updated co-ordinate system classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/Map/GameField.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/Map/GameField.java
@@ -4,13 +4,13 @@ import org.firstinspires.ftc.teamcode.Universal.Math.Pose;
 
 import java.util.ArrayList;
 
-public abstract class GameField extends FTCMap {
-    public FTCMap knownScoringElements;
+public abstract class GameField extends Map2 {
+    public Map2 knownScoringElements;
     public ArrayList<int[]> scoringElementPositions = new ArrayList<int[]>();
     public int[] numElements;
     public GameField(int[] maxScoringElements){
         super();
-        knownScoringElements = new FTCMap();
+        knownScoringElements = new Map2();
         numElements = new int[maxScoringElements.length];
         for(int maxOfType : maxScoringElements){
             scoringElementPositions.add(new int[maxOfType]);
@@ -28,6 +28,6 @@ public abstract class GameField extends FTCMap {
         scoringElementPositions = new ArrayList<int[]>();
         for(int i = 0; i < maxSizes.length; i++)
             scoringElementPositions.add(new int[maxSizes[i]]);
-        knownScoringElements = new FTCMap();
+        knownScoringElements = new Map2();
     }
 }
