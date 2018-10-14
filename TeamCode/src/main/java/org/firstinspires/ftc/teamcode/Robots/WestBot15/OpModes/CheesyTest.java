b857be5091818b4d2c3bcd5f18020e2ac1329656
Mister-Minister-Master <misterministermaster@gmail.com>
2018-10-14 14:09:53 -0400

    Revert "Changed controls in CheesyTest"
    This reverts commit 29bf7d34925fdbbcabd286485cd4c92b578e6bcf ("Changed Controls for CheesyDrive")
    
     Changes to be committed:
    	modified:   TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/CheesyTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/CheesyTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/CheesyTest.java
@@ -21,22 +21,19 @@ public class CheesyTest extends WestBot15 {
     @Override
     public void loop(){
         updateGamepad1();
+        double turnMult = (1 - leftStick1.magnitude()) * (1 - gamepad1.right_trigger) + leftStick1.magnitude()* gamepad1.right_trigger;
+        drivetrain.leftPow = leftStick1.y + turnMult * rightStick1.x;
+        drivetrain.rightPow = leftStick1.y - turnMult * rightStick1.x;
         if(gamepad1.left_stick_button)
             drivetrain.maxSpeed = 1;
         else{
             drivetrain.maxSpeed = gamepad1.left_trigger / 5.0;
             if(gamepad1.left_bumper)
+                drivetrain.maxSpeed += 0.7;
+            else if(gamepad1.right_bumper)
                 drivetrain.maxSpeed += 0.3;
             else
-                drivetrain.maxSpeed += 0.6;
+                drivetrain.maxSpeed += 0.5;
         }
-        double maxTurn = 1;
-        if(gamepad1.right_bumper)
-            maxTurn = 2.0/3;
-        double turnMult = (1 - leftStick1.magnitude() * maxTurn) * (1 - gamepad1.right_trigger) + leftStick1.magnitude() * gamepad1.right_trigger;
-        drivetrain.leftPow = leftStick1.y + turnMult * rightStick1.x;
-        drivetrain.rightPow = leftStick1.y - turnMult * rightStick1.x;
-        drivetrain.setRightPow();
-        drivetrain.setLeftPow();
     }
 }
