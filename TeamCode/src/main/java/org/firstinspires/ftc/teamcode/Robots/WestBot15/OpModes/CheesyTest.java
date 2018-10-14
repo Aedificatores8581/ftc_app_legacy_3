8dee2c1f154a1fd23ed65e3939ca5543e058adc9
theZebraBatman <fgportman00@gmail.com>
2018-10-14 17:34:44 -0400

    Fixed merge issue in CheesyTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/CheesyTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/CheesyTest.java
@@ -21,16 +21,11 @@ public class CheesyTest extends WestBot15 {
     @Override
     public void loop(){
         updateGamepad1();
-        double turnMult = (1 - leftStick1.magnitude()) * (1 - gamepad1.right_trigger) + leftStick1.magnitude()* gamepad1.right_trigger;
-        drivetrain.leftPow = leftStick1.y + turnMult * rightStick1.x;
-        drivetrain.rightPow = leftStick1.y - turnMult * rightStick1.x;
         if(gamepad1.left_stick_button)
             drivetrain.maxSpeed = 1;
         else{
-            drivetrain.maxSpeed = gamepad1.left_trigger / 5.0;
+            drivetrain.maxSpeed = gamepad1.left_trigger * 3 / 10.0;
             if(gamepad1.left_bumper)
-                drivetrain.maxSpeed += 0.7;
-            else if(gamepad1.right_bumper)
                 drivetrain.maxSpeed += 0.3;
             else
                 drivetrain.maxSpeed += 0.5;
@@ -38,9 +33,9 @@ public class CheesyTest extends WestBot15 {
         double maxTurn = 1;
         if(!gamepad1.right_bumper)
             maxTurn = 2.0/3;
-        turnMult = (1 - leftStick1.magnitude() * maxTurn) * (1 - gamepad1.right_trigger) + leftStick1.magnitude() * gamepad1.right_trigger;
-        drivetrain.leftPow = leftStick1.y + turnMult * rightStick1.x;
-        drivetrain.rightPow = leftStick1.y - turnMult * rightStick1.x;
+        drivetrain.turnMult = (1 - leftStick1.magnitude() * maxTurn) * gamepad1.right_trigger + leftStick1.magnitude() * (1 - gamepad1.right_trigger);
+        drivetrain.leftPow = leftStick1.y + drivetrain.turnMult * rightStick1.x;
+        drivetrain.rightPow = leftStick1.y - drivetrain.turnMult * rightStick1.x;
         drivetrain.setRightPow();
         drivetrain.setLeftPow();
     }
