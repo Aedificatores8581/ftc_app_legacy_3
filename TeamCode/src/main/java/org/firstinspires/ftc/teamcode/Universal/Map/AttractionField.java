bfde9da6838934ed72c8ec10ec2a8412f7f92633
theZebraBatman <fgportman00@gmail.com>
2018-10-17 07:50:47 -0400

    GRADLE!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/Map/AttractionField.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/Map/AttractionField.java
@@ -7,7 +7,7 @@ import org.firstinspires.ftc.teamcode.Universal.UniversalFunctions;
 public class AttractionField {
     public Pose location;
     public double strength;
-    //I plan to use this class for object avoidance and score-based pathfinding algorithms
+
     public AttractionField(){
         location = new Pose(0,0,0);
         strength = 1;
