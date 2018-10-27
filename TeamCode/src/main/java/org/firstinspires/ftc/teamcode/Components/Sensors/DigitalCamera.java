6e02231d65aa0cffde218e632ea5c0ac8201c9b4
theZebraBatman <fgportman00@gmail.com>
2018-10-26 22:21:32 -0400

    Fixed DigitalCamera getObjectLocation method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sensors/DigitalCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sensors/DigitalCamera.java
@@ -24,6 +24,7 @@ public class DigitalCamera {
             this.width = pixelSize * numPixelsX;
             this.height = pixelSize * numPixelsY;
         }
+
     }
     //TODO: Add magnification to calculations
     public DigitalCamera(double focalLength, double pixelSize, double resolutionX, double resolutionY){
@@ -58,18 +59,35 @@ public class DigitalCamera {
         setLocation(aCamera);
         setOrientation(aCamera);
     }
+    public void setLocationAndOrientation(Pose3 pose){
+        x = pose.x;
+        y = pose.y;
+        z = pose.z;
+        xAng = pose.rho;
+        yAng = pose.phi;
+        zAng = pose.theta;
+    }
     //TODO: Fix variable names
-    public Point getObjectLocation(Point pointOnImage, Size imageSize, double widthRatio, double heightRatio, double objectHeight){
+    public Point getObjectLocation(Point pointOnImage, Size imageSize, double objectHeight){
+        Vector2 temp = new Vector2(pointOnImage.y, -pointOnImage.x);
+        temp.x -= imageSize.height / 2;
+        temp.y -= imageSize.width / 2;
+
+        double vertAng = temp.y / imageSize.width * verticalAngleOfView();
+        double horiAng = temp.x / imageSize.height * horizontalAngleOfView();
+
+        double newY = (z - objectHeight / 2) / Math.tan(vertAng);
+        double newX = newY * Math.tan(horiAng);
+        return new Point(newX, newY);
+    }
+    public Point getObjectLocation2(Point pointOnImage, Size imageSize, double objectHeight){
         pointOnImage.x -= imageSize.width / 2;
         pointOnImage.y -= imageSize.height / 2;
         Vector2 temp = new Vector2(pointOnImage.x, pointOnImage.y);
         temp.rotate(Math.PI / 2 + yAng);
-        double theta = Math.PI / 2 + temp.y / imageSize.height * verticalAngleOfView() + xAng;
-        double rho = Math.PI / 2 + -temp.x / imageSize.width * horizontalAngleOfView() - zAng;
-        Point3 newPoint = new Point3(UniversalFunctions.sphericalToCartesian(1, theta, rho));
-        double newX = newPoint.x * (z - objectHeight) / -newPoint.z + x;
-        double newY = newPoint.y * (z - objectHeight) / -newPoint.z + y;
-        return new Point(newX, newY);
+        double theta = Math.PI / 2 - temp.y / imageSize.height * verticalAngleOfView() + xAng;
+        double rho = Math.PI / 2 - temp.x / imageSize.width * horizontalAngleOfView() - zAng;
+        return new Point((z - objectHeight / 2) * Math.tan(theta) * Math.cos(rho), (z - objectHeight / 2) * Math.tan(theta) * Math.sin(rho));
     }
     public void updateLocation(double xChange, double yChange, double zChange){
         x += xChange;
