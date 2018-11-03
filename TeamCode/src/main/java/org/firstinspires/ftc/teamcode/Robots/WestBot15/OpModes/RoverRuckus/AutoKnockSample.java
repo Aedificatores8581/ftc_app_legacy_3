2c3143ee5b6be75b205799ff8dc63394f7b659ee
mayadapples <tlovinski@outlook.com>
2018-11-03 15:50:56 -0400

    Created AutoKnockSample. Among other minor changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/RoverRuckus/AutoKnockSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/RoverRuckus/AutoKnockSample.java
@@ -4,42 +4,58 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+import org.firstinspires.ftc.teamcode.Components.Mechanisms.Drivetrains.Drivetrain;
+import org.firstinspires.ftc.teamcode.Components.Mechanisms.Drivetrains.TankDrivetrains.TankDT;
 import org.firstinspires.ftc.teamcode.Components.Sensors.Cameras.MotoG4;
+import org.firstinspires.ftc.teamcode.Robots.WestBot15.WestBot15;
+import org.firstinspires.ftc.teamcode.Universal.Math.Pose;
 import org.firstinspires.ftc.teamcode.Universal.Math.Vector2;
 import org.firstinspires.ftc.teamcode.Vision.Detectors.BlockDetector;
-import org.firstinspires.ftc.teamcode.Vision.Detectors.GenericDetector;
+import org.firstinspires.ftc.teamcode.Vision.TennisBallDetector;
 import org.opencv.core.Point;
 import org.opencv.core.Point3;
-import org.opencv.core.Size;
 
 import ftc.vision.Detector;
 
-@Autonomous(name = "Block Detector Test", group = "none")
-public class VisionTest extends OpMode {
+/**
+ * Written 13/10/18 by Theodore Lovinski.
+ *
+ * Tested: No
+ */
+
+@Autonomous(name = "KnockSample", group = "Auto Testing")
+public class AutoKnockSample extends WestBot15 {
     BlockDetector detector;
     Point sampleLocation;
     MotoG4 motoG4;
+    Pose desiredPose;
 
-    public void init(){
+    @Override
+    public void init() {
         msStuckDetectInit = 500000;
 
         motoG4 = new MotoG4();
         motoG4.setLocationAndOrientation(new Point3(0, 0, 12), new Point3(0, 0, 0));
 
         detector = new BlockDetector();
-        detector.opState = Detector.OperatingState.TUNING;
+        detector.isInitialized = true;
+        detector.opState = Detector.OperatingState.DETECTING;
 
         FtcRobotControllerActivity.frameGrabber.detector = this.detector;
-    }
-    public void initLoop() {
-        telemetry.addData("location 1", motoG4.getObjectLocationRear(detector.elements.get(0), detector.result().size(), 2));
-        telemetry.addData("location 2", motoG4.getObjectLocationRear2(detector.elements.get(0), detector.result().size(), 2));
+
+        desiredPose = new Pose();
+
+        super.init();
+        activateGamepad1();
     }
 
     @Override
     public void start() { super.start(); }
 
+    @Override
     public void loop() {
+        // updateGamepad1();
+
         Vector2 _elementVector = new Vector2(detector.element.y + 320, -detector.element.x - 240);
 
         double adjustedVerticalAngle = _elementVector.y / 640 * motoG4.verticalAngleOfViewRear();
@@ -49,14 +65,17 @@ public class VisionTest extends OpMode {
         double locationPointX = locationPointY * Math.tan(adjustedHorizontalAngle);
         Point locationPoint = new Point(locationPointX, locationPointY);
 
-        telemetry.addData("location 1", (int) (100 * locationPoint.x) / 100.0 + ", " + (int) (100 * locationPoint.y) / 100.0);
-        telemetry.addData("location 2", motoG4.getObjectLocationRear2(detector.element.clone(), detector.workingImage.size(), 2));
-        telemetry.addData("center", (float) detector.element.x + ", " + (float) detector.element.y);
-        telemetry.addData("size", detector.workingImage.size());
-        telemetry.addData("Element Vector", _elementVector);
+        drivetrain.driveToPoint(locationPoint.x, locationPoint.y, Drivetrain.Direction.FOR);
+
+        // TODO: find some way to tell that the block has been knocked.
+        // Talk to Frank about how to do this.
+        // At the moment, just detect the floor, and if red or blue is detected,
+        // then the task is complete.
+
+        telemetry.addData("Image Size", detector.workingImage.size());
     }
 
-    public void stop(){
+    public void stop() {
         super.stop();
         detector.isInitialized = false;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Detectors/BlockDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Detectors/BlockDetector.java
@@ -55,13 +55,14 @@ public class BlockDetector extends Detector {
         super();
     }
 
-    public void detect(Mat image){
+    public void detect (Mat image){
         switch(opState){
             case TUNING:
                 tune(image);
                 break;
             case DETECTING:
                 workingImage = image;
+
                 Imgproc.cvtColor(image, hsvImage, Imgproc.COLOR_RGB2HSV_FULL);
                 Core.inRange(hsvImage, new Scalar(H_MIN, S_MIN, V_MIN), new Scalar(H_MAX, S_MAX, V_MAX), threshold);
 
@@ -70,26 +71,29 @@ public class BlockDetector extends Detector {
 
                 Imgproc.erode(threshold, threshold, erosionFactor);
                 Imgproc.erode(threshold, threshold, erosionFactor);
+
                 Imgproc.dilate(threshold, threshold, dilationFactor);
                 Imgproc.dilate(threshold, threshold, dilationFactor);
+
                 Mat colSum = new Mat();
                 Core.reduce(threshold, colSum, 0, Core.REDUCE_SUM, 4);
-
-
                 break;
         }
-
     }
+
     public Mat result(){
         return workingImage;
     }
 
     public void tune(Mat image){
         Mat threshold2 = new Mat(image.size(), 0);
+
         threshold = new Mat(image.size(), 0);
         hsvImage = new Mat(image.size(), 0);
         hsv = new Mat(image.size(), 0);
+
         thresh = new Mat(image.size(), 0);
+
         i = new Mat(image.size(), 0);
         invert = new Mat(image.size(), 0);
 
@@ -104,26 +108,38 @@ public class BlockDetector extends Detector {
 */
         Imgproc.cvtColor(image, invert, Imgproc.COLOR_RGB2YUV);
         Core.inRange(invert, new Scalar(0, 0, 0), new Scalar(255, 70, 255), threshold);
+
         //Imgproc.threshold(invert.col(1), threshold, 70, 255, Imgproc.THRESH_BINARY);
+
         Mat erosionFactor = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+
         Imgproc.erode(threshold, threshold, erosionFactor);
         Imgproc.dilate(threshold, threshold, erosionFactor);
+
         Mat mask = new Mat(image.size(), 0);
         threshold.copyTo(mask);
         image.copyTo(i, mask);
 
         Imgproc.GaussianBlur(i, i, new Size(9, 9), 2, 2);
+
         //Imgproc.HoughCircles(i, i, Imgproc.CV_HOUGH_GRADIENT, 1, i.rows()/8, 100, 20, 0, 0);
+
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
         Imgproc.findContours(threshold, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
         float[] radius = new float[1];
+
         Point center = new Point();
+
         double area = -1;
         element = new Point();
+
         for (int i = 0; i < contours.size(); i++) {
             MatOfPoint c = contours.get(i);
+
             double contourArea = Imgproc.contourArea(c);
-            if(contourArea > area){
+
+            if(contourArea > area) {
                 MatOfPoint2f c2f = new MatOfPoint2f(c.toArray());
                 Imgproc.minEnclosingCircle(c2f, center, radius);
                 element = center.clone();
@@ -136,6 +152,7 @@ public class BlockDetector extends Detector {
 
 
         i.copyTo(workingImage);
+
         Imgproc.drawContours(workingImage, contours,-1, new Scalar(255,0,0),2);
         Imgproc.circle(workingImage, center, (int) radius[0], new Scalar(0, 0, 255), 5);
        // Imgproc.distanceTransform();
@@ -144,11 +161,15 @@ public class BlockDetector extends Detector {
         //Imgproc.GaussianBlur(i, i, new Size(9, 9), 2, 2);
         //Imgproc.HoughCircles(i, i, Imgproc.CV_HOUGH_GRADIENT, 1, i.rows()/8, 100, 20, 0, 0);
         threshold.release();
+
         hsvImage.release();
         hsv.release();
+
         thresh.release();
+
         i.release();
         invert.release();
+
         threshold2.release();
     }
 }
