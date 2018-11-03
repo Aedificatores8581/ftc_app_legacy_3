d826444ff6a63e232da97700dc01e4147459e1f3
mayadapples <tlovinski@outlook.com>
2018-11-02 20:07:54 -0400

    Changes I made when going through documenting.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/RoverRuckus/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/WestBot15/OpModes/RoverRuckus/VisionTest.java
@@ -22,24 +22,27 @@ public class VisionTest extends OpMode {
 
     public void init(){
         msStuckDetectInit = 500000;
+
         motoG4 = new MotoG4();
         motoG4.setLocationAndOrientation(new Point3(0, 0, 12), new Point3(0, 0, 0));
+
         detector = new BlockDetector();
         detector.opState = Detector.OperatingState.TUNING;
+
         FtcRobotControllerActivity.frameGrabber.detector = this.detector;
     }
-    public void initLoop(){
+    public void initLoop() {
         telemetry.addData("location 1", motoG4.getObjectLocationRear(detector.elements.get(0), detector.result().size(), 2));
         telemetry.addData("location 2", motoG4.getObjectLocationRear2(detector.elements.get(0), detector.result().size(), 2));
     }
+
     @Override
-    public void start(){
+    public void start() {
         super.start();
 
     }
 
-    public void loop(){
-
+    public void loop() {
         Vector2 temp = new Vector2(detector.element.y, -detector.element.x);
         temp.x -= 480/ 2;
         temp.y += 640 / 2;
@@ -51,9 +54,9 @@ public class VisionTest extends OpMode {
         double newX = newY * Math.tan(horiAng);
         Point newPoint = new Point(newX, newY);
 
-        telemetry.addData("location 1", (int)(100 * newPoint.x) / 100.0 + ", " + (int)(100 * newPoint.y) / 100.0);
+        telemetry.addData("location 1", (int) (100 * newPoint.x) / 100.0 + ", " + (int) (100 * newPoint.y) / 100.0);
         telemetry.addData("location 2", motoG4.getObjectLocationRear2(detector.element.clone(), detector.workingImage.size(), 2));
-        telemetry.addData("center", (float)detector.element.x + ", " + (float)detector.element.y);
+        telemetry.addData("center", (float) detector.element.x + ", " + (float) detector.element.y);
         telemetry.addData("size", detector.workingImage.size());
         telemetry.addData("temp", temp);
     }
