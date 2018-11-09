0c239dc4d2409b685e616042a832a7b384848b73
mayadapples <tlovinski@outlook.com>
2018-11-08 19:52:53 -0500

    Literally nothing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Mechanisms/RoverRuckus/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Mechanisms/RoverRuckus/Intake.java
@@ -7,7 +7,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Intake {
     public CRServo frontRoller, backRoller;
     public Servo dispensor;
-    //TODO: find these values
+
+    // TODO: tune these values
     public final double CLOSED_DISPENSOR_POSITION = 0, OPEN_DISPENSOR_POSITION = 1;
     public IntakeMode intakeMode;
 
@@ -17,20 +18,24 @@ public class Intake {
     public Intake(IntakeMode mode){
         intakeMode = mode;
     }
+
     public void init(HardwareMap hardwareMap){
         frontRoller = hardwareMap.crservo.get("fRol");
         backRoller = hardwareMap.crservo.get("bRol");
         dispensor = hardwareMap.servo.get("disp");
     }
+
     public double getPower(){
         return (frontRoller.getPower() + backRoller.getPower()) / 2;
     }
     //between 0 and 1
+
     public void setModePower(double pow){
         if(intakeMode == IntakeMode.GOLD)
             pow *= -1;
         setPowerDirectly(pow);
     }
+
     public void setPowerDirectly(double pow){
         frontRoller.setPower(pow);
         backRoller.setPower(pow);
@@ -39,13 +44,16 @@ public class Intake {
         else if (pow < 0)
             intakeMode = IntakeMode.GOLD;
     }
+
     public void dispense(boolean dispensing){
         dispensor.setPosition(dispensing ? OPEN_DISPENSOR_POSITION : CLOSED_DISPENSOR_POSITION);
     }
+
     enum IntakeMode{
         GOLD,
         SILVER
     }
+
     public String toString(){
         String dispensorState = "closed";
         if(dispensor.getPosition() == OPEN_DISPENSOR_POSITION)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Mechanisms/RoverRuckus/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Mechanisms/RoverRuckus/Lift.java
@@ -7,13 +7,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Lift {
     public DcMotor liftMotor;
     public Servo topRatchetServo, sideRatchetServo;
-    //TODO: find these values
-    public final double  TOP_RATCHET_ZERO_POSITION  = 0 ,
-                         TOP_RATCHET_ONE_POSITION   = 1 ,
-                         SIDE_RATCHET_ZERO_POSITION = 0 ,
-                         SIDE_RATCHET_ONE_POSITION  = 1 ;
+
+    //TODO: Tune these values.
+    public final double  TOP_RATCHET_ZERO_POSITION  = 0,
+                         TOP_RATCHET_ONE_POSITION   = 1,
+                         SIDE_RATCHET_ZERO_POSITION = 0,
+                         SIDE_RATCHET_ONE_POSITION  = 1;
     private final double TIME_TO_SWITCH_MS          = 20,
                          TICKS_PER_INCH             = 0;
+
     public double height;
     private double timer;
     RatchetState ratchetState;
@@ -23,18 +25,23 @@ public class Lift {
         switchRatchetState();
         height = 0;
     }
+
     public Lift(RatchetState ratchetState){
         this.ratchetState = ratchetState;
         switchRatchetState();
         height = 0;
     }
+
     public void init(HardwareMap hardwareMap){
         liftMotor = hardwareMap.dcMotor.get("lift");
+
         topRatchetServo = hardwareMap.servo.get("trat");
         sideRatchetServo = hardwareMap.servo.get("srat");
+
         liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+
     public void switchRatchetState(){
         resetTimer();
         switch (ratchetState){
@@ -42,59 +49,66 @@ public class Lift {
                 topRatchetServo.setPosition(TOP_RATCHET_ONE_POSITION);
                 sideRatchetServo.setPosition(SIDE_RATCHET_ZERO_POSITION);
                 break;
+
             case DOWN:
                 topRatchetServo.setPosition(TOP_RATCHET_ZERO_POSITION);
                 sideRatchetServo.setPosition(SIDE_RATCHET_ONE_POSITION);
                 break;
+
             case STOPPED:
                 topRatchetServo.setPosition(TOP_RATCHET_ZERO_POSITION);
                 sideRatchetServo.setPosition(SIDE_RATCHET_ZERO_POSITION);
                 break;
+
             case DISENGAGED:
                 topRatchetServo.setPosition(TOP_RATCHET_ONE_POSITION);
                 sideRatchetServo.setPosition(SIDE_RATCHET_ONE_POSITION);
                 break;
         }
     }
-    public void setPower(double pow){
-        if(hasSwitched())
+
+    public void setPower(double power) {
+        if(hasSwitched()) {
             switch (ratchetState) {
                 case UP:
-                    if (pow > 0)
-                        liftMotor.setPower(pow);
+                    if (power > 0) liftMotor.setPower(power);
                     break;
                 case DOWN:
-                    if (pow < 0) {
-                        liftMotor.setPower(pow);
-                    }
+                    if (power < 0) liftMotor.setPower(power);
                     break;
                 case DISENGAGED:
-                    liftMotor.setPower(pow);
+                    liftMotor.setPower(power);
                     break;
             }
+        }
     }
-    public void setPowerOverride(double pow){
-        if (pow != 0)
-            if (ratchetState != RatchetState.DISENGAGED)
-                ratchetState = pow > 0 ? RatchetState.UP : RatchetState.DOWN;
-        setPower(pow);
+
+    public void setPowerOverride(double power) {
+            if (ratchetState != RatchetState.DISENGAGED && power != 0) {
+                ratchetState = power > 0 ? RatchetState.UP : RatchetState.DOWN;
+            }
+        setPower(power);
     }
-    public double getHeight(){
+
+    public double getHeight() {
         return liftMotor.getCurrentPosition() * TICKS_PER_INCH;
     }
-    private void resetTimer(){
+    private void resetTimer() {
         timer = System.nanoTime() * 10E6;
     }
-    private boolean hasSwitched(){
+
+    private boolean hasSwitched() {
         return System.currentTimeMillis() - timer > TIME_TO_SWITCH_MS;
     }
-    enum RatchetState{
+
+    enum RatchetState {
         UP,
         DOWN,
         DISENGAGED,
         STOPPED
     }
-    public String toString(){
+
+    public String toString() {
         return ratchetState + ", " + height + " inches upwards";
     }
 }
