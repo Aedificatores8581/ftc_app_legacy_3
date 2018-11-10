7ca9a7bc3f2bd03e41b614930dfdc0f6d2f96ef9
mayadapples <tlovinski@outlook.com>
2018-11-10 17:07:40 -0500

    Created Controller folder in Universal, moved ConsectutiveButtonPress to it.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/Controller/ConsecutiveButtonPress.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Universal/Controller/ConsecutiveButtonPress.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Universal.Controller;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.ElapsedTime;
